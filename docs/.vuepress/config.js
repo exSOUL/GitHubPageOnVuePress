043c1dca95daa1492f3b13ebd32ec7a85ae19e9c
exSOUL <ex.soul.1984.cosmos@gmail.com>
2018-05-03 02:33:24 +0900

    pageとそれを生成するリポジトリに分離

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: '試したいことを試す',
+  description: 'exSOULのSandboxみたいなもの',
+  base: '/',
+  themeConfig: {
+    repo: 'exSOUL/exSOUL.github.io',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Twitter', link: 'https://twitter.com/ex_SOUL' },
+      { text: 'Qiita', link: 'https://qiita.com/ex_SOUL' },
+    ],
+    sidebar: [
+     '/',
+      '/2018/0417-1.md',
+      '/2018/0502-1.md',
+    ],
+  }
+}
